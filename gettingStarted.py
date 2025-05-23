7fc037bab2a1ffe44ee68e87fecfed3cc9d195b2
stw4114 <stw4114@nyu.edu>
2025-05-23 03:01:55 -0400

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,9 +6,9 @@ def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "Yes"
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
@@ -24,10 +24,10 @@ if __name__ == "__main__":
     print(welcome_assignment_answers(debug_question))
 
 #Questions:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-#"Are encoding and encryption the same? - Yes/No":
-#"Is it possible to decrypt a message without a key? - Yes/No":
-#"Is it possible to decode a message without a key? - Yes/No":
+#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?" pcap:
+#"Are encoding and encryption the same? - Yes/No": No
+#"Is it possible to decrypt a message without a key? - Yes/No": Yes
+#"Is it possible to decode a message without a key? - Yes/No": Yes
 #"Is a hashed message supposed to be un-hashed? - Yes/No":
 #"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
 #"Is MD5 a secured hashing algorithm? - Yes/No":
